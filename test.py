41eeb22fe8f746bd91aea8198720d9693cc3cb77
CyprienIUT <cpe.albert2@gmail.com>
2024-03-11 11:48:54 +0100

    update SAE 2.02

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+from random import randint
+from math import ceil
+
 WIDTH = HEIGHT = 5
 COUPS_CAVALIERS = [(-2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2)]
 
@@ -21,8 +24,8 @@ def backtracking(x, y, chemin=None, chemins=None):
 
     if (len(chemin) == WIDTH*HEIGHT and chemin not in chemins):
         chemins.append(chemin[:])
-        afficher_tableau(chemin)
-        print(f"Chemin {len(chemins)} ajoutés")
+        # afficher_tableau(chemin)
+        # print(f"Chemin {len(chemins)} ajoutés")
     
     coups = coups_possible(x, y, chemin)
 
@@ -45,13 +48,39 @@ def coup_possible(x,  y, chemin):
     return (0 <= x < WIDTH and 0 <= y < HEIGHT and (x, y) not in chemin)
 
 tous_chemins = {}
-for x in range(WIDTH) :
-    for y in range(HEIGHT) :
-        tous_chemins[(x, y)] = backtracking(x, y)
-        print(res)
+for x in range(ceil(WIDTH/2)) :
+    for y in range(ceil(HEIGHT/2)) :
+        res = backtracking(x, y)
+        tous_chemins[(x, y)] = res
+        print(f"La case ({x}, {y}) a {len(tous_chemins[(x, y)])} chemins hamiltoniens heuristiques")
+        if len(tous_chemins[(x, y)]) != 0:
+            print("Voici un chemin aléatoire parmi tous les chemins possibles : ")
+            afficher_tableau(tous_chemins[(x, y)][randint(0, len(tous_chemins[(x, y)]))-1])
+        print("Calcul de la symétrie")
+        tous_chemins[(symetrie_axiale_point_x(x), y)] = symetrie_axiale_chemins_x(res)
+        tous_chemins[(X, symetrie_axiale_chemin_y(y))] = symetrie_axiale_chemins_y(res)
+        
+        
 
 
 # print(f"({x}, {y}) > ({next_coup[0]}, {next_coup[1]}) = {len(chemin)} cases remplies")
 # afficher_tableau(chemin)
 # print("\n")
 
+def symetrie_axiale_point_x(x):
+    ...
+
+def symetrie_axiale_point_y(y):
+    ...
+
+def symetrie_axiale_chemin_x(chemin):
+    ...
+
+def symetrie_axiale_chemin_y(chemin):
+    ...
+
+def symetrie_axiale_chemins_x(chemins):
+    ...
+
+def symetrie_axiale_chemins_y(chemins):
+    ...
